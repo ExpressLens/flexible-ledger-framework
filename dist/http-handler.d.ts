a5ef6aea651df2ec955f86d33746bf45e1f2bd41
ExpressLens <expresslens@proton.me>
2022-05-14 09:21:32 +0000

    cosmit

--- dist/http-handler.d.ts
+++ dist/http-handler.d.ts
@@ -1,1 +1,12 @@
-import { EventHandler } fr
+import { EventHandler } from '.';
+/**
+ * @deprecated Please use the EventHandler
+ * Incase your request is a string, parse it
+ * before calling the EventHandler.
+ * @see https://github.com/rabobank-blockchain/universal-ledger-agent/issues/11
+ */
+export declare class HttpHandler {
+    private eventHandler;
+    constructor(eventHandler: EventHandler);
+    handleRequest(request: any, callback: any): Promise<void>;
+}
