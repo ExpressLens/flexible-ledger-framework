8f0b4a643aaffac1461387d9a2682e2736796236
ExpressLens <expresslens@proton.me>
2022-05-20 15:53:56 +0000

    Create travis.yml

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -15,4 +15,14 @@
  */
 
 export { EventHandler } from './event-handler';
-export { HttpHandler } from './http-ha
+export { HttpHandler } from './http-handler'
+export { UlaMessage, Message } from './model/ula-message'
+export { IAttestation, Attestation } from './model/attestation'
+export { IAttestor, Attestor } from './model/attestor';
+export { ITransaction, Transaction } from './model/transaction';
+export { IUlaResponse, UlaResponse } from './model/ula-response';
+export { PluginResult } from './model/plugin-result';
+export { UlaError } from './model/ula-error';
+export { Plugin } from './interface/plugin';
+export { HttpService } from './interface/http-service';
+export { UlaCallback } from './interface/ula-ca
