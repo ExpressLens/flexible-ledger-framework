5dabb2dedfc6ba2d5a01af29b61fb8c41f9c76dc
ExpressLens <expresslens@proton.me>
2022-07-04 06:33:50 +0000

    fix merge error

--- dist/model/attestation.d.ts
+++ dist/model/attestation.d.ts
@@ -9,4 +9,24 @@ export interface IAttestation {
     statements: any;
 }
 /**
- * All data source
+ * All data sources and ULA plugins
+ * must eventually transform their
+ * attestations / credentials back
+ * to this model.
+ */
+export declare class Attestation {
+    private readonly _uuid;
+    private readonly _attestorPubKey;
+    private readonly _forPubKey?;
+    private readonly _context;
+    private readonly _type;
+    private readonly _expires?;
+    private readonly _datetime;
+    private readonly _statements;
+    constructor(attestation: IAttestation);
+    /**
+     * The uuid of this attestation
+     * @return string
+     */
+    get uuid(): string;
+    /*
