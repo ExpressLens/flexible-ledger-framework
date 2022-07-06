718ec6404185ad25d7aa06abda5c81e2e373b4f4
ExpressLens <expresslens@proton.me>
2022-07-06 17:52:15 +0000

    correct style

--- dist/model/attestor.d.ts
+++ dist/model/attestor.d.ts
@@ -1,3 +1,25 @@
 import { IAttestation, Attestation } from './attestation';
 import { ITransaction, Transaction } from './transaction';
-export i
+export interface IAttestor {
+    name: string;
+    icon: string;
+    pubKey: string;
+    datetime: Date | string;
+    transactions?: ITransaction[];
+    receivedAttestations?: IAttestation[];
+    issuedAttestations?: IAttestation[];
+}
+/**
+ * All data sources and ULA plugins
+ * must eventually transform their
+ * attestors / issuers  back to this
+ * model.
+ */
+export declare class Attestor {
+    private readonly _name;
+    private readonly _icon;
+    private readonly _pubKey;
+    private readonly _datetime;
+    private _transactions?;
+    private _receivedAttestations?;
+    private _issuedAtte
