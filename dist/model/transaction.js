079af6d434ea4002ecf9f2b359db18b664e25a0a
ExpressLens <expresslens@proton.me>
2022-09-17 10:47:20 +0000

    updated readme links

--- dist/model/transaction.js
+++ dist/model/transaction.js
@@ -33,4 +33,18 @@
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(targe
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", {value: true})
+exports.Transaction = void 0
+const attestation_1 = require("./attestation");
+const class_transformer_1 = require("class-transformer");
+const uuid_1 = require("uuid");
+/**
+ * A Transaction is a way of recording data
+ * transfers. The holder can either receive
+ * or send data from/to other parties.
+ * It is possible to send and receive data
+ * in one transaction. In that case, attest
+ * and verifyRequest are
