14779f9aa20ec07a38ee7d5da09d031b3de21f8d
ExpressLens <expresslens@proton.me>
2022-09-27 15:43:24 +0000

    Add a readme

---
+++ dist/service/browser-http-service.d.ts
@@ -0,0 +1,16 @@
+import { HttpService } from '..';
+export declare class BrowserHttpService implements HttpService {
+    /**
+     * Perform a JSON GET-request and wait
+     * for response
+     * @param {string} url
+     * @return Object the json object response
+     */
+    getRequest(url: string): Promise<object>;
+    /**
+     * Perform a POST-request and get the json contents back
+     * @param {string} url
+     * @param {string} postBody
+     * @return Object json object
+     */
+    postRequest(url: string, postBody: obj
