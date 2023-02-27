9a557668addeca95cbce89a6ed9449e7322af428
ExpressLens <expresslens@proton.me>
2023-02-27 09:49:51 +0000

    Licenses

--- test/unit/attestor/attestor-getters.test.ts
+++ test/unit/attestor/attestor-getters.test.ts
@@ -8,4 +8,20 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { assert } from 'chai'
+import { Attestation, Attestor } from '../../../src'
+
+const testData = {
+  name: 'string',
+  icon: 'string',
+  pubKey: 'string',
+  datetime: (new Date()).toISOString()
+  // transactions?: Transaction[]
+  // receivedAttestations?: Attestation[]
+  // issuedAttesta
