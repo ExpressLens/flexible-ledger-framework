bcaf046379cf2e1083673d9930365668125e5aee
ExpressLens <expresslens@proton.me>
2023-03-09 20:08:54 +0000

    Bump to next dev version

--- test/unit/ula-error/ula-error-constructor.test.ts
+++ test/unit/ula-error/ula-error-constructor.test.ts
@@ -7,4 +7,21 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless requ
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { assert } from 'chai'
+import { UlaError } from '../../../src'
+
+const testData = {
+  statusCode: 'error-code',
+  message: 'Something went wrong!'
+}
+
+describe('UlaError constructor', function () {
+
+  it('should not throw on valid
